fd0867d571a1e6d5c579d2ef34a4a211f73cc9be
Peter Fu <peter.fu@activecollabmod.com>
2014-08-11 11:06:29 +0800

    添加了JPA的使用例子

---
+++ app/cn/edu/sdu/sc/spepms/system/creation/forms/PublishProjectForm.java
@@ -0,0 +1,15 @@
+package cn.edu.sdu.sc.spepms.system.creation.forms;
+
+public class PublishProjectForm {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,7 +1,14 @@
 package controllers;
 
+
+import java.util.List;
+
 import cn.edu.sdu.sc.spepms.framework.wireframe.Wireframe;
+import cn.edu.sdu.sc.spepms.system.creation.forms.PublishProjectForm;
+import cn.edu.sdu.sc.spepms.system.creation.models.PublishedProject;
 import play.*;
+import play.data.Form;
+import play.db.jpa.JPA;
 import play.mvc.*;
 import views.html.*;
 
@@ -32,6 +39,34 @@ public class Application extends Controller {
 
     public static Result saveArticle() {
         return redirect(routes.Application.index());
+    }
+    
+    public static Result dummySavePublishedProject() {
+        // 从提交的表单中获取数据
+        Form<PublishProjectForm> form = Form.form(PublishProjectForm.class).bindFromRequest();
+        PublishProjectForm data = form.get();
+        
+        // 往PublishedProject表中添加一条记录
+        PublishedProject p = new PublishedProject();
+        p.setDescription(data.getName());
+        JPA.em().persist(p);
+        
+        // 查找PublishedProject表里面的所有记录
+        List<PublishedProject> publishedProjectList = JPA.em().createQuery("from PublishedProject", PublishedProject.class).getResultList();
+        
+        // 更新某条记录
+        Long id = 1L;
+        PublishedProject someP = JPA.em().find(PublishedProject.class, id);
+        someP.setDescription(data.getName());
+        JPA.em().merge(someP);
+        
+        // 删除，需要确认一下是那一个
+        JPA.em().remove(id);
+        JPA.em().remove(someP);
+        
+        
+        
+        return null;
     }
 
 }
